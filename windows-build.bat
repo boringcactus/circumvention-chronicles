630d4731f5f9a1d399b770e50cd5a96c4eccadd9
Melody <boringcactus@gmail.com>
2018-02-28 21:23:44 -0600

    actually fix Windows build script for realsies

--- windows-build.bat
+++ windows-build.bat
@@ -1,7 +1,8 @@
 cargo build --release
+rmdir /S /Q dist
 mkdir dist
 mv target\release\circumvention-chronicles.exe dist
-copy assets dist\
+robocopy /S assets dist/assets
 butler -V
 butler.exe push dist boringcactus/circumvention-chronicles:windows-jam
 
